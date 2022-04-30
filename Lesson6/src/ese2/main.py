397ded6dbea17a5692cc543a0e179a3befff1bde
Nick <nick.iselle@outlook.com>
2022-04-30 19:10:51 +0200

    Added exercise 2

--- Lesson6/src/ese2/main.py
+++ Lesson6/src/ese2/main.py
@@ -1,20 +1,30 @@
 # Exercise 2 Lesson 6
 
-def invert_dict(d : dict()):
-    keysList = []
+def invert_dict(d):
+    inverse = {}
+    keysList = [] 
     itemsList = []
-    for k, i in d.items():
-        if k not in keysList:
-            keysList.append(k)
-            itemsList.append(i)
+    for k in d:
+        keysList.append(k)
+
+    for i in d:
+        itemsList.append(d[i])
     
-    inverse = dict()
+    for i in range(len(d)):
+        itemsListCpy = itemsList.copy()
+        itemsListCpy.pop(itemsListCpy.index(itemsList[i]))
+        if itemsList[i] not in itemsListCpy:
+            inverse.setdefault(itemsList[i], keysList[i])
+        else:
+            raise ValueError("unique inversion is not possible")
 
-    for item in itemsList:
-        inverse[item] = keysList[item - 1]
-    return inverse 
+    return inverse
 
-numeri = {'uno':1, 'due':2, 'tre':3}
-
+numeri = {'uno': 1, 'due': 2, 'tre': 3}
+gambe = {'uomo': 2, 'gatto': 4, 'lampada': 1, 'cane': 4}
+print(numeri)
 inverseNum = invert_dict(numeri)
 print(inverseNum)
+print(gambe)
+inverseGamb = invert_dict(gambe)
+print(inverseGamb)
